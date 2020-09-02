47b6e1844ef05e2e72828b4ce5ddcc244a07c518
Wayne <waynejeng@aetheras.io>
2020-09-02 17:34:17 +0800

    [Change] Follow Google Shell Style Guide

--- venv_setup.sh
+++ venv_setup.sh
@@ -1,39 +1,41 @@
 #!/bin/sh
+#
+# Setup virtual environment.
 
 # Variables
 VENV="env"
 REQ="requirements.txt"
 
 main () {
-	venv_create
-	venv_activate
+  venv_create
+  venv_activate
 }
 
 # Create virtual environment
 venv_create() {
-	if ! check_exist "${VENV}"; then
-		python3 -m venv ${VENV}
-		info "Virtual environment created"
-	else
-		info "Virtual environment existed"
-	fi
+  if ! check_exist "${VENV}"; then
+    python3 -m venv ${VENV}
+    info "Virtual environment created"
+  else
+    info "Virtual environment existed"
+  fi
 }
 
 # Activate virtual environment
 venv_activate() {
-	if check_exist "${VENV}"; then
-		source ${VENV}/bin/activate
-		info "Virtual environment activated"
-	else
-		info "Unable to find virtual environment"
-		exit 1
-	fi
+  if check_exist "${VENV}"; then
+    source ${VENV}/bin/activate
+    info "Virtual environment activated"
+  else
+    info "Unable to find virtual environment"
+    exit 1
+  fi
 
-	# Install packages by requirements file
-	if check_exist "${REQ}"; then
-		pip3 install -r ${REQ}
-		info "Packages installed"
-	fi
+  # Install packages by requirements file
+  if check_exist "${REQ}"; then
+    pip3 install -r ${REQ}
+    info "Packages installed"
+  fi
 }
 
 #
@@ -53,24 +55,24 @@ CYAN='\033[0;36m'
 WHITE='\033[0;37m'
 
 info() {
-	printf "\r  [ ${BLUE}..${NC} ] $1\n"
+  pintf "\r  [ ${BLUE}..${NC} ] $1\n"
 }
 
 ok() {
-	printf "\r${CLEAR}  [ ${GREEN}OK${NC} ] $1\n"
+  printf "\r${CLEAR}  [ ${GREEN}OK${NC} ] $1\n"
 }
 
 err() {
-	printf "\r${CLEAR}  [ ${RED}ERR${NC} ] $1\n"
-	exit
+  printf "\r${CLEAR}  [ ${RED}ERR${NC} ] $1\n"
+  exit
 }
 
 check_cmd() {
-	command -v "$1" >/dev/null 2>&1
+  command -v "$1" >/dev/null 2>&1
 }
 
 check_exist() {
-	command ls "$1" >/dev/null 2>&1
+  command ls "$1" >/dev/null 2>&1
 }
 
 main "$@"
