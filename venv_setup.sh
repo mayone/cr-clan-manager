2a6711fe4a505e2e5d4b015e52261252ecb1836a
Wayne <waynejeng@aetheras.io>
2020-09-02 16:05:44 +0800

    [Change] Combine utils and venv setup into one file

--- venv_setup.sh
+++ venv_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-. ./utils.sh
-
 # Variables
 VENV="env"
 REQ="requirements.txt"
@@ -38,4 +36,41 @@ venv_activate() {
 	fi
 }
 
-main
+#
+# Utils
+#
+
+# Colors
+CLEAR='\033[2K'
+NC='\033[0m'
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+info() {
+	printf "\r  [ ${BLUE}..${NC} ] $1\n"
+}
+
+ok() {
+	printf "\r${CLEAR}  [ ${GREEN}OK${NC} ] $1\n"
+}
+
+err() {
+	printf "\r${CLEAR}  [ ${RED}ERR${NC} ] $1\n"
+	exit
+}
+
+check_cmd() {
+	command -v "$1" >/dev/null 2>&1
+}
+
+check_exist() {
+	command ls "$1" >/dev/null 2>&1
+}
+
+main "$@"
