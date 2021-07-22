4cb8ec4689460d3e24034a684f8bae3b098f6de2
mayone <mayone321@gmail.com>
2021-07-22 15:19:45 +0800

    [Change] Remove redundant code

--- utils/singleton.py
+++ utils/singleton.py
@@ -9,22 +9,3 @@ class Singleton(type):
             cls._instances[cls] = super(
                 Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
-
-
-class cls_testing(metaclass=Singleton):
-    def __init__(self):
-        self.__test_str = "Singleton test FAILED!!!"
-
-    def set_str(self, str):
-        self.__test_str = str
-
-    def get_str(self):
-        return self.__test_str
-
-
-def singleton_test():
-    a = cls_testing()
-    b = cls_testing()
-    a.set_str("Singleton test SUCCESS!!!")
-    result = b.get_str()
-    print(result)
